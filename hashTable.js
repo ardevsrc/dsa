4d9a4b8326bf9d08d105c7eaba7188ceef5de33f
ardevsrc <dbeast@tuta.io>
2022-03-08 21:11:10 -0500

    Added get method

--- hashTable.js
+++ hashTable.js
@@ -1,7 +1,23 @@
 class HashTable {
+
   constructor(size) {
     this.size = size;
     this.table = [];
+  }
+
+  get(key) {
+    const index = this.hash(key);
+    const bucket = this.table[index];
+
+    if (bucket) {
+      for (let i = 0; i < bucket.length; ++i) {
+        if(bucket[i].key === key) {
+          return bucket[i].value;
+        }
+      }
+    }
+
+    return undefined;
   }
 
   
